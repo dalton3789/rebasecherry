d26b19c0d60986a6c9410f4d9da06c4bfe2144e6
Dalton <toan.nguyen@finstro.com>
2022-12-14 21:18:01 +0700

    Add PickView

---
+++ GitCherry/GitCherry/PickView.swift
@@ -0,0 +1,26 @@
+//
+//  PickView.swift
+//  GitCherry
+//
+//  Created by Toan Nguyen on 12/14/22.
+//
+
+import SwiftUI
+
+struct PickView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct PickView_Previews: PreviewProvider {
+    static var previews: some View {
+        PickView()
+    }
+}
