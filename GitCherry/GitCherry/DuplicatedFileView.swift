bfb31aa1c8bd266b2648d11499721eebc9b84e09
Dalton <toan.nguyen@finstro.com>
2022-12-14 21:41:04 +0700

    Add duplicated file

---
+++ GitCherry/GitCherry/DuplicatedFileView.swift
@@ -0,0 +1,27 @@
+//
+//  DuplicatedFileView.swift
+//  GitCherry
+//
+//  Created by Toan Nguyen on 12/14/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct DuplicatedFileView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct DuplicatedFileView_Previews: PreviewProvider {
+    static var previews: some View {
+        DuplicatedFileView()
+    }
+}
