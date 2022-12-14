d244c6f41ea8293159b36bb87645f004483707a6
Dalton <toan.nguyen@finstro.com>
2022-12-14 17:52:18 +0700

    Init project

---
+++ GitCherry/GitCherry/GitCherryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitCherryApp.swift
+//  GitCherry
+//
+//  Created by Toan Nguyen on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct GitCherryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
